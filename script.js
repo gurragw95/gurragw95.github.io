0929faa3600fefc39c8cef3324ab7fbe912922b3
gurragw95 <145570398+gurragw95@users.noreply.github.com>
2024-11-20 15:45:07 +0000

    Changed to that menu disappears when link is clicked

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let phoneMenuButton;
 let phoneMenuDiv;
+//Navbaren
 const navDiv = document.querySelector("#navDiv");
+//Menyraderna med länkar
+const navLinks = document.querySelectorAll(".navLinks");
 
 
 //######### TELEFONSPECIFIKT ##################
@@ -12,27 +15,46 @@ phoneMenuDiv = document.querySelector("#phoneMenuDiv");
 //Event listeners
 document.addEventListener("click", (e)=>{
     //Menyn för telefoner. Om menyn är i visat läge, så stängs den, om tvärt om så öppnas den.
+    //"Scrolled" är en CSS-klass där den grå färgen används som bakgrund
     if (phoneMenuButton.contains(e.target)){
         if(phoneMenuDiv.style.display === "none"){
             phoneMenuDiv.style.display = "flex";
-            navDiv.style.backgroundColor = "#f1f5f9";
+            navDiv.classList.add("scrolled");
         } else{
             phoneMenuDiv.style.display = "none";
-            navDiv.style.backgroundColor = "transparent";
+            navDiv.classList.remove("scrolled");
         }
         
-    };
+    }
+    
 });
 
 //Scroll detection. När man scrollat ner med telefonen ner till "information", så blir navbaren inte längre osynlig
-
 const summaryHeaderDiv = document.querySelector("#summaryHeaderDiv");
 
 document.addEventListener("scroll", ()=>{
     const sectionTop = summaryHeaderDiv.offsetTop;
     if(window.scrollY >= sectionTop){
         navDiv.classList.add("scrolled");
-    } else{
+    }else if(window.scrollY < sectionTop && phoneMenuDiv.style.display === "flex"){
         navDiv.classList.remove("scrolled");
+        phoneMenuDiv.style.display = "none";
     }
-})
+    else{
+        phoneMenuDiv.style.display = "none";
+        navDiv.classList.remove("scrolled");     
+     
+    }
+});
+
+//Gör att menyn försvinner efter man klickat på en länk. FUNKAR INTE!
+for(let element of navLinks){
+    element.addEventListener("click", ()=>{
+        phoneMenuDiv.style.display = "none";
+    });
+};
+
+
+
+
+
